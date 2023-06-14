3aa3a0cc6dbb95bb37e6a1be4a124340dcc7b08e
bubaololo <bubaololo@gmail.com>
2023-06-14 15:35:55 +0600

    profile

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,20 +2,21 @@ const routeNames = {
   'home': '/',
   'register': '/register',
   'login': '/login',
+  'profile.edit': '/profile',
   'vehicles.index': '/vehicles',
   'parkings.active': '/parkings/active',
 }
-
+ 
 function route(name, params = {}) {
   let url = routeNames[name]
-
+ 
   for (let prop in params) {
     if (Object.prototype.hasOwnProperty.call(params, prop)) {
       url = url.replace(`:${prop}`, params[prop])
     }
   }
-
+ 
   return url
 }
-
+ 
 export { route }
